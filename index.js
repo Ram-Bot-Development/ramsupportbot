30314fa564e3ae9522fac6498df2c98161ee8958
Gamearoo Codes <gamearoo10@gmail.com>
2022-12-23 18:40:17 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const ConsoleLog = require("./Funtions/log");
+const Log = require("./Funtions/log");
+
+
+
+new ConsoleLog("SUPPORT").Ginfo(";p")
+
+setInterval(() => new ConsoleLog("SUPPORT").Ginfo(";sddasdasdsadap"), 15000)
